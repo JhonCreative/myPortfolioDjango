17d6100c5c7aa4142021945a3b8a150b6bed08ee
Jhon Calsina <jhoncalsina51@email.com>
2024-04-08 20:14:14 -0400

    Add context whit info

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# def about_view(request):
+#    my_profile={
+#       'name':'Jhon Calsina'
+#    }
+#    return render(request, 'index.html', {'my_profile':
+#       my_profile})
+
+
+
+class index(TemplateView):
+   template_name = 'index.html'
+   def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['name'] = 'Jhon Calsina'
+      context['saludo'] = 'hola como estas'
+      return context
